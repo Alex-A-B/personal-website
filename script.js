0ac1d4b759730f2bc342756cafbfa20eb3639df4
Alex-A-B <alexjamesbelton@gmail.com>
2021-04-06 08:51:43 +0100

    background pic added, html update, css and js linkages amended

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 const h2 = document.createElement('h2');
-h2.textContent = "This content added by JavaScript";
+h2.textContent = "This content added by JavaScript.";
 
-document.querySelector('body').appendChild(h2);
+document.querySelector('div#principal').appendChild(h2);
