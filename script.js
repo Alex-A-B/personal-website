87496932b116970c90d48191b954f68ad099b5fa
Alex-A-B <alexjamesbelton@gmail.com>
2021-04-06 10:48:11 +0100

    Projects update

--- script.js
+++ script.js
@@ -1,3 +1,32 @@
+const dodger = document.getElementById("dodger"); 
+
+function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+  } 
+
+document.addEventListener("keydown", function(e) {
+    if (e.key === "ArrowLeft") {
+      moveDodgerLeft();
+    }
+    if (e.key === "ArrowRight") {
+      moveDodgerRight();
+    }
+  }); 
+  
+  function moveDodgerRight() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+
+    if (left <= 359) {
+        dodger.style.left = `${left + 1}px`
+    }
+  }
+
 const h2 = document.createElement('h2');
 h2.textContent = "This content added by JavaScript.";
 
