afe83b092908965410966b53f2660953cda39d7b
Alex-A-B <alexjamesbelton@gmail.com>
2021-04-09 11:23:22 +0100

    Nav bar added with script - style  to work on

--- script.js
+++ script.js
@@ -39,3 +39,31 @@ document.querySelector('div#principal').appendChild(h2);
 // JS show preview on mousehover
 
 // JS show/hide onClick
+
+// this is yoinked from a jquery class - reverse engineer to a js version
+/*
+$(document).ready(function(){
+$(".hide_menu").click(function(){
+    $('#menu_div').css("display","none");
+});
+
+$(".show_menu").click(function(){
+    $('#menu_div').css("display","initial");
+});
+});
+
+*/
+
+// Navigation bits
+
+var body = document.getElementsByTagName("body")[0];
+var navigation = document.getElementById("navigation");
+
+window.addEventListener("scroll", function(evt) {
+  if (body.scrollTop > navigation.getBoundingClientRect().bottom) {
+    // when the scroll's y is bigger than the nav's y set class to fixednav
+    navigation.className = "fixednav"
+  } else { // Overwise set the class to staticnav
+    navigation.className = "staticnav"
+  }
+});
